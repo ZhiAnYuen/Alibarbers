31defee2f6291af79b418f4d495f69b2c2b6acdf
Yuen Zhi An <yuenzhian@gmail.com>
2022-10-05 21:34:44 +0800

    Add ESLint and Prettier

--- src/main.js
+++ src/main.js
@@ -1,29 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDGx9onhnjCYGfE-nizcuGc6JckEkf6jpw",
-	authDomain: "alibarber-9dfc5.firebaseapp.com",
-	projectId: "alibarber-9dfc5",
-	storageBucket: "alibarber-9dfc5.appspot.com",
-	messagingSenderId: "358955090781",
-	appId: "1:358955090781:web:4cd91fd7529781a1cd5eb4",
-	measurementId: "G-CP0XDG5FDV",
-};
+    apiKey: 'AIzaSyDGx9onhnjCYGfE-nizcuGc6JckEkf6jpw',
+    authDomain: 'alibarber-9dfc5.firebaseapp.com',
+    projectId: 'alibarber-9dfc5',
+    storageBucket: 'alibarber-9dfc5.appspot.com',
+    messagingSenderId: '358955090781',
+    appId: '1:358955090781:web:4cd91fd7529781a1cd5eb4',
+    measurementId: 'G-CP0XDG5FDV',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
-const col = collection(db, "shop");
-const snapshot = await getDocs(col);
-const list = snapshot.docs.map((doc) => doc.data());
+const col = collection(db, 'shop')
+const snapshot = await getDocs(col)
+const list = snapshot.docs.map((doc) => doc.data())
 
-console.log(list[0]);
-document.getElementById("ownerName").innerText = list[0]["ownerName"];
-document.getElementById("shopName").innerText = list[0]["shopName"];
-document.getElementById("location").innerText = list[0]["location"];
-console.log(list[0]["services"]);
-
-
-// document.getElementById("list").innerText = list[0];
+console.log(list[0])
+console.log(list[0]['ownerName'])
+console.log(list[0]['shopName'])
+console.log(list[0]['location'])
+console.log(list[0]['services'])
