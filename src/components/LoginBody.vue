bb035d71a9e32c18467d9209fa55c8f2f10a73d1
Yuen Zhi An <yuenzhian@gmail.com>
2022-10-07 01:17:11 +0800

    Add LoginView

---
+++ src/components/LoginBody.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="login-body" class="container-fluid vh-100"></div>
+</template>
+
+<script>
+export default {
+  name: "LoginBody",
+};
+</script>
+
+<style scoped>
+* {
+  background-color: #ffe69a;
+}
+
+#login-body {
+  background-image: url("../assets/scissor.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 1100px;
+}
+
+/* .bg-image {
+  background-image: url("../assets/scissor.png");
+  background-repeat: no-repeat;
+  background-position: center;
+} */
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,18 @@
 <template>
   <main>
     <HomeHeader />
+    <LoginBody />
   </main>
 </template>
 
 <script>
 import HomeHeader from "../components/HomeHeader.vue";
+import LoginBody from "../components/LoginBody.vue";
 
 export default {
   components: {
     HomeHeader,
+    LoginBody,
   },
 };
 </script>
