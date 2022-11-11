0f0d760ee199a36022d0aef68f143d8e049f8db6
natworking <nataliechan.2021@scis.smu.edu.sg>
2022-11-11 17:28:17 +0800

    add sections below

--- src/components/HomeBody.vue
+++ src/components/HomeBody.vue
@@ -16,6 +16,61 @@
       </div>
     </div>
   </div>
+
+  <div class="container-fluid vh-100 customer">
+    <div class="row align-items-center vh-100">
+      <div class="align-self-center">
+        <h5>For Customers</h5>
+        <h3>Enjoy Easy, Convenient & Accessible Hair Services</h3>
+        <div class="row justify-content-center">
+          <div class="col-3 text-center whiteBox">
+            Box 1
+          </div>
+          <div class="col-3 text-center whiteBox">
+            Box 2
+          </div>
+          <div class="col-3 text-center whiteBox">
+            Box 3
+          </div>
+        </div>
+        <button
+          type="button"
+          class="hover-button button mt-4 mb-4 mx-auto d-block"
+          v-on:click="$router.push('/signup')"
+        >
+          Get Started
+        </button>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="container-fluid vh-100 hairdresser">
+      <div class="row align-items-center vh-100">
+        <div class="align-self-center">
+        <h5>For Hairdressers</h5>
+        <h3>Expand Your Reach & Generate Sales</h3>
+        <div class="row justify-content-center">
+          <div class="col-3 text-center whiteBox">
+            Box 1
+          </div>
+          <div class="col-3 text-center whiteBox">
+            Box 2
+          </div>
+          <div class="col-3 text-center whiteBox">
+            Box 3
+          </div>
+        </div>
+        <button
+          type="button"
+          class="hover-button button mt-4 mb-4 mx-auto d-block"
+          v-on:click="$router.push('/signup')"
+        >
+          Join Us
+        </button>
+      </div>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,5 +82,31 @@ export default {
 <style lang="scss" scoped>
 h1 {
   font-size: 60px;
+}
+
+h3{
+  font-weight: bold;
+}
+
+h5{
+  text-decoration: underline;
+  font-weight: bold;
+}
+
+.customer {
+  background-color: #ffe69a;
+  
+}
+
+.hairdresser {
+  background-color: #92b4ec;
+}
+
+.whiteBox {
+  background-color: white;
+  width: 20em;
+  height: 15em;
+  margin: 1em;
+  border-radius: 20%;
 }
 </style>
