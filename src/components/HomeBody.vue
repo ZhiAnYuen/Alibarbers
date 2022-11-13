14183d3ecd24b94986a9345edad1e37b8ccf0b88
ShermanNovak <sk33t0722@gmail.com>
2022-11-13 14:14:22 +0800

    updated home page

--- src/components/HomeBody.vue
+++ src/components/HomeBody.vue
@@ -1,162 +1,152 @@
 <template>
-  <div class="container-fluid vh-100">
-    <div class="row align-items-center vh-100">
-      <div class="col-md-6 col-sm-12 align-self-center">
-        <h1 class="text-center col-12">Singapore's No. 1</h1>
-        <h1 class="text-center col-12">Hair Marketplace</h1>
-        <h3 class="text-center text-muted m-4">Handling All Your Hair Needs</h3>
+  <div class="container-fluid">
+    <div class="row">
+      <div
+        class="col-md-6 col-sm-12 align-self-center p-5 d-flex flex-column justify-content-evenly"
+      >
+        <h1>Singapore's No. 1 Hair Marketplace</h1>
+        <p class="fs-5">Handling All Your Hair Needs</p>
         <div class="row justify-content-center">
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <button
-              type="button"
-              class="hover-button button mt-4 mb-4 mx-auto d-block"
-            >
+          <div class="col-lg-6 col-xs-12">
+            <a href="#customer-section" class="hover-button button">
               For Customers
-            </button>
+            </a>
           </div>
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <button
+          <div class="col-lg-6 col-xs-12">
+            <a
+              href="#hairdresser-section"
               type="button"
-              class="hover-button button mt-4 mb-4 mx-auto d-block"
+              class="hover-button button ms-auto"
             >
               For Hairdressers
-            </button>
+            </a>
           </div>
         </div>
       </div>
-      <div class="col-md-6 col-sm-12 align-self-center text-center">
+      <div class="col-md-6 col-sm-12 align-self-center">
         <img class="img-fluid" src="../assets/main-image.PNG" alt="..." />
       </div>
     </div>
   </div>
 
-  <div class="container-fluid vh-100" id="customer">
-    <div class="row align-items-center vh-110">
-      <div class="align-self-center">
-        <h5 class="colouredHeader">For Customers</h5>
-        <h3 class="colouredSubHeader">
-          Enjoy Easy, Convenient & Accessible Hair Services
-        </h3>
+  <div class="container-fluid" id="customer-section">
+    <div class="p-5">
+      <u class="text-start fw-semibold">For Customers</u>
+      <h3 class="text-start fw-semibold mt-2">
+        Enjoy Easy, Convenient & Accessible Hair Services
+      </h3>
 
-        <div class="row justify-content-center">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                <img src="../assets/icons8-earth-globe-100.png" class="image" />
-              </h5>
-              <h6 class="card-title mb-4"><br /><b>Explore a New World</b></h6>
-              <p class="card-text">
-                Get all the hair services you want in one place. Explore new
-                hair salons and hairdressers to find what works best for you!
-              </p>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                <img src="../assets/booking.png" class="image" />
-              </h5>
-              <h6 class="card-title mb-4"><br /><b>Speedy Bookings</b></h6>
-              <p class="card-text">
-                Book appointments without the hassle of calling up the salon
-                itself. Keep track of your upcoming appointments and add them to
-                your google calendar!
-              </p>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                <img
-                  src="../assets/icons8-ask-question-100.png"
-                  class="image"
-                />
-              </h5>
-              <h6 class="card-title mb-4"><br /><b>Communicate via Chat</b></h6>
-              <p class="card-text">
-                Chat with hairdressers to clarify any doubts before you pay for
-                the services!
-              </p>
-            </div>
+      <div class="row mt-4 mx-0">
+        <div class="col-lg-4 col-sm-12 card rounded-4">
+          <div class="card-body">
+            <h5 class="card-title">
+              <img src="../assets/icons8-earth-globe-100.png" class="image" />
+            </h5>
+            <h6 class="card-title"><b>Explore a New World</b></h6>
+            <p class="card-text">
+              Get all the hair services you want in one place. Explore new hair
+              salons and hairdressers to find what works best for you!
+            </p>
           </div>
         </div>
 
-        <button
-          type="button"
-          class="hover-button button mt-4 mb-4 mx-auto d-block"
-          style="position: absolute; left:4em"
-          v-on:click="$router.push('/signup')"
-        >
-          Get Started
-        </button>
+        <div class="col-lg-4 col-sm-12 card rounded-4">
+          <div class="card-body">
+            <h5 class="card-title">
+              <img src="../assets/booking.png" class="image" />
+            </h5>
+            <h6 class="card-title"><b>Speedy Bookings</b></h6>
+            <p class="card-text">
+              Book appointments without the hassle of calling up the salon
+              itself. Keep track of your upcoming appointments and add them to
+              your google calendar!
+            </p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-12 card rounded-4">
+          <div class="card-body">
+            <h5 class="card-title">
+              <img src="../assets/icons8-ask-question-100.png" class="image" />
+            </h5>
+            <h6 class="card-title"><b>Communicate via Chat</b></h6>
+            <p class="card-text">
+              Chat with hairdressers to clarify any doubts before you pay for
+              the services!
+            </p>
+          </div>
+        </div>
       </div>
+      <button
+        type="button"
+        class="hover-button button me-auto mt-4"
+        v-on:click="$router.push('/signup')"
+      >
+        Get Started
+      </button>
     </div>
   </div>
   <div>
-    <div class="container-fluid vh-100" id="hairdresser">
-      <div class="row align-items-center vh-110">
-        <div class="align-self-center">
-          <h5 class="colouredHeader" s>For Hairdressers</h5>
-          <h3 class="colouredSubHeader">Expand Your Reach & Generate Sales</h3>
-          <div class="row justify-content-center">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">
-                  <img src="../assets/icons8-customer-96.png" class="image" />
-                </h5>
-                <h6 class="card-title mb-4"><br /><b>Allocate Manpower</b></h6>
-                <p class="card-text">
-                  Receive appointments online instead of via call. View all your
-                  scheduled appointments easily using our calendar.
-                </p>
-              </div>
-            </div>
-
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">
-                  <img
-                    src="../assets/icons8-communication-100.png"
-                    class="image"
-                  />
-                </h5>
-                <h6 class="card-title mb-4">
-                  <br /><b>Clarify Doubts Directly</b>
-                </h6>
-                <p class="card-text">
-                  Directly reach out to customers via our chat function to
-                  resolve any queries or issues. Receive direct feedback on your
-                  services after appointments!
-                </p>
-              </div>
-            </div>
-
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">
-                  <img
-                    src="../assets/icons8-combo-chart-100.png"
-                    class="image"
-                  />
-                </h5>
-                <h6 class="card-title mb-4"><br><b>Store Analytics</b></h6>
-                <p class="card-text">
-                  Analyse how well your store is doing with our dashboards.
-                </p>
-              </div>
+    <div class="container-fluid" id="hairdresser-section">
+      <div class="p-5">
+        <div class="d-flex flex-column flex-end">
+          <u class="text-end fw-semibold">For Hairdressers</u>
+          <h3 class="text-end fw-semibold mt-2">
+            Expand Your Reach & Generate Sales
+          </h3>
+        </div>
+        <div class="row d-flex flex-row mx-0 mt-4">
+          <div class="col-lg-4 col-sm-12 card rounded-4">
+            <div class="card-body">
+              <h5 class="card-title">
+                <img src="../assets/icons8-customer-96.png" class="image" />
+              </h5>
+              <h6 class="card-title"><b>Allocate Manpower</b></h6>
+              <p class="card-text">
+                Receive appointments online instead of via call. View all your
+                scheduled appointments easily using our calendar.
+              </p>
             </div>
           </div>
-          <button
-            type="button"
-            class="hover-button button mt-4 mb-4 mx-auto d-block"
-            style="position: absolute; right:4em"
-            v-on:click="$router.push('/signup')"
-          >
-            Join Us
-          </button>
+
+          <div class="col-lg-4 col-sm-12 card rounded-4">
+            <div class="card-body">
+              <h5 class="card-title">
+                <img
+                  src="../assets/icons8-communication-100.png"
+                  class="image"
+                />
+              </h5>
+              <h6 class="card-title">
+                <b>Clarify Doubts Directly</b>
+              </h6>
+              <p class="card-text">
+                Directly reach out to customers via our chat function to resolve
+                any queries or issues. Receive direct feedback on your services
+                after appointments!
+              </p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-sm-12 card rounded-4">
+            <div class="card-body">
+              <h5 class="card-title">
+                <img src="../assets/icons8-combo-chart-100.png" class="image" />
+              </h5>
+              <h6 class="card-title"><br /><b>Store Analytics</b></h6>
+              <p class="card-text">
+                Analyse how well your store is doing with our dashboards.
+              </p>
+            </div>
+          </div>
         </div>
+        <button
+          type="button"
+          class="hover-button button ms-auto mt-4"
+          v-on:click="$router.push('/signup')"
+        >
+          Join Us
+        </button>
       </div>
     </div>
   </div>
@@ -169,50 +159,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  font-size: 3em;
-  font-weight: bold;
+#customer-section {
+  background-color: $pastel-yellow;
 }
 
-h3 {
-  font-weight: bold;
-}
-
-h5 {
-  text-decoration: underline;
-  font-weight: bold;
-}
-
-#customer {
-  background-color: #ffe69a;
-}
-
-#hairdresser {
-  background-color: #92b4ec;
+#hairdresser-section {
+  background-color: $blue;
 }
 
 .image {
   height: 4em;
   width: 4em;
   margin: 4px;
-}
-
-.card {
-  border-radius: 15%;
-  width: 20em;
-  height: 20em;
-  margin: 2em;
-}
-
-.colouredHeader {
-  margin-left: 4rem;
-  margin-right: 4rem;
-  margin-top: 2rem;
-  margin-bottom: 1rem;
-}
-
-.colouredSubHeader {
-  margin-left: 4rem;
-  margin-right: 4rem;
 }
 </style>
