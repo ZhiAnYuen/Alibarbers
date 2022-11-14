d31481d78e7ed59feb98f73005730631f8bc06f1
natworking <nataliechan.2021@scis.smu.edu.sg>
2022-11-14 17:05:06 +0800

    route chat to shop

--- src/components/ChatBody.vue
+++ src/components/ChatBody.vue
@@ -72,26 +72,38 @@
             <!-- CHAT HEADER -->
             <div class="row">
               <div id="chatInfo">
-                <h4 class="m-2 text-white">
-                  <svg
-                    id="convoListButton"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    fill="currentColor"
-                    class="bi bi-list me-3"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                    />
-                  </svg>
+                <div class="row">
+                  <h4 class="m-2 text-white col-lg-9 col-md-6">
+                    <svg
+                      id="convoListButton"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      fill="currentColor"
+                      class="bi bi-list me-3"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                      />
+                    </svg>
 
-                  {{ chatHeaderName }}
-                </h4>
+                    {{ chatHeaderName }}
+                  </h4>
+
+                  <button
+                    v-if="chatHeaderName != ''"
+                    id="visitShopBtn"
+                    class="col-lg-2 hover-button p-2"
+                    style="border-color: white; margin-right: 0"
+                    @click="routeToShop"
+                  >
+                    Visit Shop
+                  </button>
+                </div>
               </div>
             </div>
 
@@ -250,6 +262,10 @@ export default {
         });
       }
       this.inputText = "";
+    },
+    routeToShop() {
+      console.log(this.currentUID)
+      this.$router.push({ path: "/shop/" + this.otherUID });
     },
   },
 };
